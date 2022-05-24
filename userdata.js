9bfa36ee5ef3d6f36d07ddab4b1d56ab3ba7f406
HizbullahBhatti <78537759+HizbullahBhatti@users.noreply.github.com>
2022-05-24 16:28:16 +0500

    Add files via upload
    "second commit"

---
+++ userdata.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const userdata = mongoose.Schema(
+    {
+       name:
+       {
+            type:String,
+            required:true
+        },
+        email:
+       {
+            type:String,
+            required:true
+        },
+        password:
+        {
+            type:String,
+            required:true
+        }, 
+        image:
+        {
+            type:String,
+            required:true
+        },
+        phone:
+        {
+            type:String,
+            required:true
+        },
+        city:
+        {
+            type:String,
+            required:true
+            
+        },
+        postcode:
+        {
+            type:Number,
+            required:true
+        }
+    }
+);
+
+const Regstr = mongoose.model('registration',userdata);
+module.exports = Regstr
